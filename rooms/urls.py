0273566a1443ecb5863f5d96540754435b47841f
kangraemin <remin1994@gmail.com>
2020-09-29 23:58:16 +0900

    Use viewsets for creating CRUD functions automatically

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,18 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import views
+
+# from . import views
+from . import viewsets
 
 app_name = "rooms"
 
-urlpatterns = [
-    path("list/", views.ListRoomsView.as_view()),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
-]
+router = DefaultRouter()
+router.register("", viewset=viewsets.RoomViewset, basename="room")
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     # path("list/", views.ListRoomsView.as_view()),
+#     # path("<int:pk>/", views.SeeRoomView.as_view()),
+# ]
